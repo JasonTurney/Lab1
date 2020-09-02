43e50665cc70534564106bcd814e76791b7f01be
Jason Turney <jnt5211@psu.edu>
2020-09-02 19:14:18 +0000

    1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ temp = float(input("Enter temperature: "))
 choice = str(input("Enter unit in F/f or C/c: "))
 if choice == 'f' or choice == 'F':
   celsius = (temp - 32) * 5/9
-  print(temp, "° in Farenheit is equivalent to ", celsius, "° Celsius.", sep='')
+  print(temp, "° in Fahrenheit is equivalent to ", celsius, "° Celsius.", sep='')
 if choice == 'c' or choice == 'C':
   faren = (temp * 9/5) + 32
   print(temp, "° in Celsius is equivalent to ", faren, "° Fahrenheit.", sep='')
